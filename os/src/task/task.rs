1bd25413a86ce7f4a22ca14781ff82354c11e2a7
gsk <gsk22@mails.tsinghua.edu.cn>
2026-01-03 21:46:33 +0800

    ch3

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,6 +1,7 @@
 //! Types related to task management
 
 use super::TaskContext;
+use crate::config::SYSCALL_ID_UPPER_BOUND;
 
 /// The task control block (TCB) of a task.
 #[derive(Copy, Clone)]
@@ -9,6 +10,8 @@ pub struct TaskControlBlock {
     pub task_status: TaskStatus,
     /// The task context
     pub task_cx: TaskContext,
+    ///
+    pub syscall_cnt: [usize; SYSCALL_ID_UPPER_BOUND],
 }
 
 /// The status of a task
